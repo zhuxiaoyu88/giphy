e3eeb10a605b1dcdb63bd93fa300c7ce6d6242c0
Anita <46235179+zhuxiaoyu88@users.noreply.github.com>
2022-06-11 16:38:50 +0800

    Finalize Homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,30 @@
 import { Link } from "react-router-dom";
+import GiphyLogo from "../components/GiphyLogo";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to Your Giphy</h1>
-      <Link to="/list">Iron Man Giphy</Link>
-      <br />
-      <Link to="/search">Search Your Giphy</Link>
-    </div>
+    <main className="container">
+      <div className="main-heading main-heading--home">
+        <h1 className="main-heading__title text-uppercase text-center">
+          Welcome to Your Giphy
+        </h1>
+        <a
+          href="https://giphy.com/"
+          className="main-heading--home__logo"
+          target="_blank"
+          rel="nofollow"
+        >
+          <GiphyLogo />
+        </a>
+        <ul className="nav-home text-uppercase text-center">
+          <li className="nav-home__item">
+            <Link to="/list">Iron Man Giphy</Link>
+          </li>
+          <li className="nav-home__item">
+            <Link to="/search">Search Your Giphy</Link>
+          </li>
+        </ul>
+      </div>
+    </main>
   );
 }
